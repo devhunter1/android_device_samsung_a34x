aeeddbce49953eae81a844673e507d82c1d85993
devhunter1 <anubhabkundu2@gmail.com>
2024-08-18 13:40:41 +0530

    a34x : TWRP device tree bringup

---
+++ twrp_a34x.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from a34x device
+$(call inherit-product, device/samsung/a34x/device.mk)
+
+PRODUCT_DEVICE := a34x
+PRODUCT_NAME := twrp_a34x
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A346E
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
