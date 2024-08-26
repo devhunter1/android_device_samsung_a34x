fd3a3cc2b8033986d1cc525177563969950f4062
xnnnsets <167276632+xnnnsets@users.noreply.github.com>
2024-08-26 15:55:39 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -39,19 +39,21 @@ fi
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
         export ALLOW_MISSING_DEPENDENCIES="true"
         export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
-        export FOX_ENABLE_APP_MANAGER=1
+        export FOX_ENABLE_APP_MANAGER="1"
+        export FOX_USE_LZ4_BINARY="1"
+        export FOX_USE_ZSTD_BINARY="1"
+        export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
         export OF_SCREEN_H="2340"
         export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
         export LC_ALL="C"
         export FOX_USE_NANO_EDITOR="1"
-        export FOX_DELETE_AROMAFM="1"
         export FOX_USE_TAR_BINARY="1"
         export FOX_USE_SED_BINARY="1"
         export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
         export FOX_USE_XZ_UTILS="1"
         export FOX_VARIANT="A14"
         export OF_MAINTAINER="xnnnsets"
-        export FOX_BUILD_TYPE="Unstable"
+        export FOX_BUILD_TYPE="Dick Yeah"
         export FOX_VERSION="R12.1"
         export OF_FL_PATH1="/tmp/flashlight" # See /*.rc for more information
 fi
